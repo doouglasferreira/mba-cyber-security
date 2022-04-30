8114d629a8ec4a74fdee6aca8901beb41b22766c
doouglasferreira <66528129+doouglasferreira@users.noreply.github.com>
2022-04-30 16:25:34 -0300

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,19 @@
+#!/usr/bin/env groovy
+ 
+pipeline {
+    //Agent é o NÓ que vai rodar o job
+    agent any
+ 
+    //Fases do pipeline
+    stages {
+        
+       stage('Checkout') {
+            steps {
+                script {
+                    echo 'fazer checkout do projeto'
+                        
+                }
+            }
+        }
+     }
+}
