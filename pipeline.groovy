aed90ce5a57e9f7390a0f83fc33e135434f0789b
doouglasferreira <66528129+doouglasferreira@users.noreply.github.com>
2022-04-30 16:50:36 -0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -7,15 +7,53 @@ pipeline {
     //Fases do pipeline
     stages {
         
-       stage('Checkout') {
+       stage('build') {
             steps {
                 script {
-                    echo 'fazer checkout do projeto'
+                    echo 'fazer build do projeto'
                     sh "ls -la"
                     sh "pwd"
                         
                 }
             }
         }
-     }
+        stage('unit-test') {
+            steps {
+                script {
+                    echo 'fazendo teste do projeto'
+                    sh "ls -la"
+                    sh "pwd"
+                        
+                }
+            }
+        } 
+    
+         stage('deploy to stage') {
+            steps {
+                script {
+                    echo 'fazendo o deploy do projeto'
+                    sh "ls -la"
+                    sh "pwd"
+                        
+                }
+            }
+        }
+         stage('acceptance test') {
+            steps {
+                script {
+                    echo 'acceptance test projeto'
+                    sh "ls -la"
+                    sh "pwd"
+                        
+                }
+            }
+        }
+    
+     stage('deploy to prodution') {
+            steps {
+                script {
+                    echo 'deploy to prodution do projeto'
+                    sh "ls -la"
+                    sh "pwd"
+    }
 }
